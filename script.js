dc38baf21227bc878860bab2d819c995ceada376
PinkIr1s <155625655+PinkIr1s@users.noreply.github.com>
2024-07-27 12:47:01 +1000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("download-link").addEventListener("click", (event) => {
-    const link = document.createElement("a");
-    link.href = "Bean-Regular.ttf"
-    link.download = "Bean-Regular.ttf"
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-});
